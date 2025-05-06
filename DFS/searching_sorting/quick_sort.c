941ad0622167f85dee062c4863d72a61af458293
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2025-05-06 14:52:20 +0530

    Create quick_sort.c

---
+++ DFS/searching_sorting/quick_sort.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; // choosing the last element as pivot
+    int i = (low - 1); // index of smaller element
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1); // return the partition point
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high); // pi is partitioning index
+
+        quickSort(arr, low, pi - 1); // sort elements before partition
+        quickSort(arr, pi + 1, high); // sort elements after partition
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    quickSort(arr, 0, n - 1);
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
