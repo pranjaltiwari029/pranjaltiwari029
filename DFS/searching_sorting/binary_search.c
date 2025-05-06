f16c93b5b04b60060a09549f0e5ae15f58e0315b
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2025-05-06 14:51:34 +0530

    Create binary_search.c

---
+++ DFS/searching_sorting/binary_search.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2; 
+
+        if (arr[mid] == key)
+            return mid; 
+        else if (arr[mid] < key)
+            low = mid + 1; 
+        else
+            high = mid - 1; 
+    }
+
+    return -1; 
+}
+
+int main() {
+    int n, key;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d sorted elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+
+    int result = binarySearch(arr, n, key);
+
+    if (result == -1) {
+        printf("Element not found in the array.\n");
+    } else {
+        printf("Element found at index %d.\n", result);
+    }
+
+    return 0;
+}
